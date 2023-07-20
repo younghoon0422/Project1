8fde65e6c24a906bce7b88e4ae63707b04f1b886
Kimyounghoon <younghun422@gmail.com>
2023-07-20 14:33:09 +0900

    두 번째 커밋 빼기함수 추가

--- main.py
+++ main.py
@@ -2,11 +2,22 @@
 def add_func(n1,n2):
     return n1+n2
 
+def add_func(n1,n2):
+    return n1-n2
+
 ##전역 변수부
 num1,num2,res = 100,200,0
 
 ##메인 코드부
 res = add_func(num1,num2)
 print(num1,'+',num2,'=',res)
+
+
+
+
+res = add_func(num1,num2)
+print(num1,'-',num2,'=',res)
+
+
 
 
